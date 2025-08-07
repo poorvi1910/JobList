1353901dde3d413e34bb41099d8f345622e7ecbe
Poorvi Kanodia <poorvikanodia19@gmail.com>
2025-08-07 23:46:33 +0530

    Used links for everything

--- joblist/src/App.jsx
+++ joblist/src/App.jsx
@@ -1,8 +1,15 @@
 import HomePage from './pages/Homepage'
+import MainLayout from './layouts/MainLayout'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
+import JobsPage from './pages/JobsPage'
 
 const router=createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+    </Route>
+  )
 )
 const App = () => {
   return <RouterProvider router={router} />
--- joblist/src/pages/HomePage.jsx
+++ joblist/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import Hero from '../components/Hero'
+import HomeCard from '../components/HomeCard'
+import JobListings from '../components/JobListings'
+import ViewAallJobs from '../components/ViewAllJobs'
 const HomePage = () => {
   return (
     <>
     <Hero />
+    <HomeCard />
+    <JobListings />
+    <ViewAallJobs />
     </>
   )
 }
